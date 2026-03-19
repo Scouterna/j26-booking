c0b8b90ecf00e692b73768261071f613b1f32ea9
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2026-03-19 22:26:50 +0100

    chore: set up dev script for hot reloading

--- dev.sh
+++ dev.sh
@@ -6,5 +6,22 @@ if [ -f .env.sh ]; then
   . ./.env.sh
 fi
 
-cd client && gleam run -m lustre/dev build --minify --outdir=../server/priv/static
-cd ../server && gleam run
+# cd client && gleam run -m lustre/dev build --minify --outdir=../server/priv/static
+
+# Start both processes in parallel so they can run together.
+(cd client && gleam run -m lustre/dev start) &
+client_pid=$!
+
+(cd server && gleam run) &
+server_pid=$!
+
+cleanup() {
+  trap - INT TERM EXIT
+  kill "$client_pid" "$server_pid" 2>/dev/null || true
+  wait "$client_pid" "$server_pid" 2>/dev/null || true
+}
+
+# Ensure Ctrl+C (INT) and termination signals stop both processes.
+trap cleanup INT TERM EXIT
+
+wait "$client_pid" "$server_pid"
